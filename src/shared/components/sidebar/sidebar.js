0bd94cd89454e640fca94b8c934c59fa1712ca91
Luis Felipe <2511550630@fema.edu.br>
2026-06-05 23:03:58 -0300

    feat: implementar responsividade para sidebar

--- src/shared/components/sidebar/sidebar.js
+++ src/shared/components/sidebar/sidebar.js
@@ -1,12 +1,86 @@
-const pageTitle = document.querySelector("title");
-const sidebarItems = document.querySelectorAll("#desktop-sidebar ul li");
+const pageTitle = document.querySelector('title')
+const sidebar = document.getElementById('sidebar')
+const sidebarLogo = document.getElementById('sidebar-logo')
+const sidebarItems = document.querySelectorAll('#sidebar ul li')
+const sidebarItemsLinks = document.querySelectorAll('#sidebar ul li a')
+const toggleSidebar = document.querySelectorAll('.toggle-mobile-sidebar')
+const sidebarText = document.querySelectorAll('.collapsable-text')
+const mobileSidebarMask = document.querySelector('.sidebar-mobile-mask')
+const collapseSidebar = document.getElementById('collapse-side-bar')
+const uncollapseSidebar = document.getElementById('uncollapse-side-bar')
+const logoutButton = document.getElementById('logout-button')
 
-document.querySelectorAll("#desktop-sidebar ul li a").forEach((link, index) => {
-  if (link.innerText === pageTitle.innerText) {
-    sidebarItems[index].classList.add("bg-[var(--sidebar-item-focus)]");
-  } else if (
-    sidebarItems[index].classList.contains("bg-[var(--sidebar-item-focus)]")
-  ) {
-    sidebarItems[index].classList.remove("bg-[var(--sidebar-item-focus)]");
+const toggleMaskStyles = () => {
+  if (mobileSidebarMask.classList.contains('hidden')) {
+    mobileSidebarMask.classList.toggle('hidden')
+    setTimeout(() => {
+      mobileSidebarMask.classList.toggle('opacity-0')
+    }, 0)
+  } else {
+    setTimeout(() => {
+      mobileSidebarMask.classList.toggle('hidden')
+    }, 150)
+    mobileSidebarMask.classList.toggle('opacity-0')
   }
-});
+}
+
+const collapseSidebarFunction = () => {
+  sidebarText.forEach((element) => {
+    element.classList.toggle('lg:hidden')
+  })
+
+  collapseSidebar.classList.toggle('hidden')
+
+  if (!uncollapseSidebar.classList.contains('hidden')) {
+    uncollapseSidebar.classList.toggle('hidden')
+  }
+
+  sidebarItemsLinks.forEach((element) => {
+    element.classList.toggle('lg:justify-center')
+  })
+
+  logoutButton.classList.toggle('lg:justify-center')
+
+  sidebar.classList.toggle('lg:w-fit')
+}
+
+uncollapseSidebar.addEventListener('click', collapseSidebarFunction)
+
+collapseSidebar.addEventListener('click', collapseSidebarFunction)
+
+// sidebarLogo.addEventListener('mouseover', () => {
+//   sidebarLogo.classList.toggle('hidden')
+//   if (sidebar.classList.contains('w-fit-important')) {
+//     uncollapseSidebar.classList.toggle('hidden')
+//   } else {
+//     collapseSidebar.classList.toggle('hidden')
+//   }
+// })
+
+// sidebarLogo.addEventListener('mouseout', () => {
+//   sidebarLogo.classList.toggle('hidden')
+// })
+
+mobileSidebarMask.addEventListener('click', () => {
+  document.getElementById('sidebar').classList.toggle('translate-x-full')
+
+  toggleMaskStyles()
+})
+
+toggleSidebar.forEach((element) => {
+  element.addEventListener('click', () => {
+    document.getElementById('sidebar').classList.toggle('translate-x-full')
+
+    toggleMaskStyles()
+  })
+})
+
+sidebarItemsLinks.forEach((link, index) => {
+  if (link.innerText === pageTitle.innerText) {
+    sidebarItems[index].classList.add('bg-[var(--sidebar-item-focus)]')
+  } else if (
+    sidebarItems[index].classList.contains('bg-[var(--sidebar-item-focus)]')
+  ) {
+    sidebarItems[index].classList.remove('bg-[var(--sidebar-item-focus)]')
+  }
+})
--- src/shared/components/sidebar/sidebar.php
+++ src/shared/components/sidebar/sidebar.php
@@ -8,74 +8,109 @@ $idUsuarioLogado = $_SESSION['usuario-logado'];
 $dal = new UsuarioDal();
 $usuario = $dal->findById($idUsuarioLogado);
 ?>
-<nav
-  id="desktop-sidebar"
-  class="w-100 h-screen text-white flex flex-col bg-[var(--sidebar-bg-color)] border-r border-r-gray-800">
-  <div class="flex items-center p-3 gap-2">
-    <div
-      class="relative before:shadow-[0px_0px_30px_10px_var(--main-color)] before:bottom-1/2 before:absolute before:right-1/2">
-      <img class="w-10 h-10" src="/images/icon.svg" alt="" />
-    </div>
+<aside class="text-white">
+  <div class="w-full flex flex-row items-center lg:hidden bg-[var(--sidebar-bg-color)] border-b border-b-gray-800 p-3 gap-4">
+    <i class="toggle-mobile-sidebar cursor-pointer rounded hover:bg-[var(--main-bg-color)] inline-flex bi bi-layout-sidebar-inset-reverse text-lg p-3 transition-colors"></i>
 
-    <div class="flex">
-      <h4 class="font-bold">Chip</h4>
-      <h4 class="text-[var(--main-color)] font-bold">Store</h4>
+    <div class="flex items-center gap-2">
+      <div
+        class="relative before:shadow-[0px_0px_30px_10px_var(--main-color)] before:bottom-1/2 before:absolute before:right-1/2">
+        <img class="w-10 h-10" src="/images/icon.svg" alt="" />
+      </div>
+
+      <div class="flex">
+        <h4 class="font-bold">Chip</h4>
+        <h4 class="text-[var(--main-color)] font-bold">Store</h4>
+      </div>
     </div>
   </div>
+  <nav
+    id="sidebar"
+    class="absolute top-0 right-full lg:static w-screen sm:w-4/5 lg:w-100 h-screen flex flex-col bg-[var(--sidebar-bg-color)] border-r border-r-gray-800 transition-all lg:translate-x-0 z-10">
+    <div class="sidebar-header flex items-center justify-center p-3 gap-2">
+      <div
+        id="sidebar-logo" class="relative before:shadow-[0px_0px_30px_10px_var(--main-color)] before:bottom-1/2 before:absolute before:right-1/2">
+        <img class="w-10 h-10" src="/images/icon.svg" alt="" />
+      </div>
 
-  <div class="p-3 border-y border-y-gray-800 flex-1">
-    <h5 class="text-sm ml-2 mb-2 text-gray-400">Gerenciamento</h5>
-    <ul class="list-none flex flex-col gap-2">
-      <li class="sidebar-item rounded-lg transition-all cursor-pointer">
-        <a class="flex-1 p-2 flex gap-2" href="/view/">
-          <i
-            style="display: inline-flex"
-            class="fa fa-cubes basis-5 justify-center items-center"></i>
-          Dashboard</a>
-      </li>
-      <li class="sidebar-item rounded-lg transition-all cursor-pointer">
-        <a class="flex-1 p-2 flex gap-2" href="/view/modules/produto/">
-          <i
-            style="display: inline-flex"
-            class="fa fa-box basis-5 justify-center items-center"></i>
-          Produtos</a>
-      </li>
-      <li class="sidebar-item rounded-lg transition-all cursor-pointer">
-        <a class="flex-1 p-2 flex gap-2" href="/view/modules/cliente/">
-          <i
-            style="display: inline-flex"
-            class="fa fa-user-group basis-5 justify-center items-center"></i>
-          Clientes</a>
-      </li>
-      <li class="sidebar-item rounded-lg transition-all cursor-pointer">
-        <a class="flex-1 p-2 flex gap-2" href="/view/modules/pedido/">
-          <i
-            style="display: inline-flex"
-            class="fa fa-cart-shopping basis-5 justify-center items-center"></i>
-          Pedidos</a>
-      </li>
-      <li class="sidebar-item rounded-lg transition-all cursor-pointer">
-        <a class="flex-1 p-2 flex gap-2" href="/view/modules/item-pedido/">
-          <i
-            style="display: inline-flex"
-            class="fa fa-list-ol basis-5 justify-center items-center"></i>Itens de Pedido</a>
-      </li>
-    </ul>
-  </div>
+      <div class="flex collapsable-text">
+        <h4 class="font-bold">Chip</h4>
+        <h4 class="text-[var(--main-color)] font-bold">Store</h4>
+      </div>
 
-  <div class="flex flex-col gap-4 p-3">
-    <div>
-      <?php if (isset($usuario)): ?>
-        <h3 class="font-semibold"><?php echo $usuario->getNome() ?></h3>
-        <h4 class="text-sm text-gray-400"><?php echo $usuario->getEmail() ?></h4>
-      <?php
-      endif; ?>
+      <div id="collapse-side-bar" class="flex items-center justify-end flex-1">
+        <i class="inline-flex bi bi-chevron-bar-left cursor-pointer rounded hover:bg-[var(--main-bg-color)] p-3 transition-colors"></i>
+      </div>
+      <div id="uncollapse-side-bar" class="hidden items-center justify-end flex-1">
+        <i class="inline-flex bi bi-chevron-bar-right cursor-pointer rounded hover:bg-[var(--main-bg-color)] p-3 transition-colors"></i>
+      </div>
+      <div class="toggle-mobile-sidebar lg:hidden flex items-center justify-end flex-1">
+        <i class="cursor-pointer rounded hover:bg-[var(--main-bg-color)] inline-flex bi bi-layout-sidebar-inset text-lg p-3 transition-colors"></i>
+      </div>
     </div>
-    <a
-      href="../../../view/actions/logout.php"
-      class="flex items-center gap-3 py-2 px-4 rounded-lg bg-[var(--main-bg-color)] cursor-pointer border border-gray-800 hover:bg-[var(--main-bg-color-hover)] transition-all ease-in-out delay-100">
-      <i class="fa fa-arrow-right-from-bracket"></i>
-      Sair
-    </a>
-  </div>
-</nav>
+
+    <div class="p-3 border-y border-y-gray-800 flex-1">
+      <h5 class="collapsable-text text-sm ml-2 mb-2 text-gray-400">Gerenciamento</h5>
+      <ul class="list-none flex flex-col gap-2">
+        <li class="sidebar-item rounded-lg transition-all cursor-pointer">
+          <a class="flex-1 p-3 flex gap-2" href="/view/">
+            <i
+              style="display: inline-flex"
+              class="fa fa-cubes basis-5 justify-center items-center"></i>
+            <p class="collapsable-text">Dashboard</p>
+          </a>
+        </li>
+        <li class="sidebar-item rounded-lg transition-all cursor-pointer">
+          <a class="flex-1 p-3 flex gap-2" href="/view/modules/produto/">
+            <i
+              style="display: inline-flex"
+              class="fa fa-box basis-5 justify-center items-center"></i>
+            <p class="collapsable-text">Produtos</p>
+          </a>
+        </li>
+        <li class="sidebar-item rounded-lg transition-all cursor-pointer">
+          <a class="flex-1 p-3 flex gap-2" href="/view/modules/cliente/">
+            <i
+              style="display: inline-flex"
+              class="fa fa-user-group basis-5 justify-center items-center"></i>
+            <p class="collapsable-text">Clientes</p>
+          </a>
+        </li>
+        <li class="sidebar-item rounded-lg transition-all cursor-pointer">
+          <a class="flex-1 p-3 flex gap-2" href="/view/modules/pedido/">
+            <i
+              style="display: inline-flex"
+              class="fa fa-cart-shopping basis-5 justify-center items-center"></i>
+            <p class="collapsable-text">Pedidos</p>
+          </a>
+        </li>
+        <li class="sidebar-item rounded-lg transition-all cursor-pointer">
+          <a class="flex-1 p-3 flex gap-2" href="/view/modules/item-pedido/">
+            <i
+              style="display: inline-flex"
+              class="fa fa-list-ol basis-5 justify-center items-center"></i>
+            <p class="collapsable-text">Itens de Pedido</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="flex flex-col justify-center gap-4 p-3">
+      <div class="collapsable-text">
+        <?php if (isset($usuario)): ?>
+          <h3 class="font-semibold"><?php echo $usuario->getNome() ?></h3>
+          <h4 class="text-sm text-gray-400"><?php echo $usuario->getEmail() ?></h4>
+        <?php
+        endif; ?>
+      </div>
+      <a
+        id="logout-button"
+        href="../../../view/actions/logout.php"
+        class="flex items-center gap-3 p-3 rounded-lg bg-[var(--main-bg-color)] cursor-pointer border border-gray-800 hover:bg-[var(--main-bg-color-hover)] transition-all ease-in-out delay-100">
+        <i class="fa fa-arrow-right-from-bracket"></i>
+        <p class="collapsable-text">Sair</p>
+      </a>
+    </div>
+  </nav>
+  <div class="sidebar-mobile-mask hidden absolute left-0 top-0 w-screen opacity-0 h-screen lg:hidden bg-black/50 transition-opacity"></div>
+</aside>
